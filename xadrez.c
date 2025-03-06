d31eb8c652a1be2468f54bf8200c3f4e15cdb64e
kevinLdias <kevinhg2222@gmail.com>
2025-03-06 14:06:50 +0000

    Implementando a movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,40 +5,52 @@ int main() {
     int movTorre = 5, movBispo = 5, movRainha = 8;
 
     // movimentação da torre
-    printf("TORRE: ");
+    printf("\nTORRE:\n");
     for (int i = 0; i < movTorre; i++)
     {
         printf("Direita");
         if (i < movTorre - 1) {
-            printf("...");
+            printf("...\n");
         }
-        else { printf("\n"); }
+        else { printf("(parou)\n"); }
     }
 
     // movimentação bispo
-    printf ("BISPO: ");
+    printf ("\nBISPO:\n");
     int cont = 0;
     while (cont < movBispo) {
         printf("Cima, direita");
         cont++;
         if (cont < movBispo) {
-            printf("...");
+            printf("...\n");
         }
-        else { printf("\n"); }
+        else { printf("(parou)\n"); }
     }
 
     // movimentação rainha
-    printf("RAINHA: ");
+    printf("\nRAINHA:\n");
     cont = 0;
     do {
         printf("Esquerda");
         cont++;
         if (cont < movRainha) {
-            printf("...");
+            printf("...\n");
         }
-        else { printf("\n"); }
+        else { printf("(parou)\n"); }
     }
     while (cont < movRainha);
+
+    // Implementando a movimentação do cavalo
+    int movCavalo = 1;
+    int j = 1;
+    printf("\nCAVALO:\n");
+    for (int i = 1; i <= movCavalo; i++) {
+        printf("Baixo...Baixo...");
+        while (j <= i) {
+            printf("Esquerda(parou)\n");
+            j++;
+        }
+    }
 
     return 0;
 }
